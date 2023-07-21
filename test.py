1d306e9c31281b1c3cfd4d13537fd05881da19b5
aq1sept <vadim-petrosyan@ukr.net>
2023-07-21 13:58:01 +0300

    updated test.py

--- test.py
+++ test.py
@@ -1,12 +1,33 @@
-import json
+import re
 
-# Read the content of the .acf file
-with open("steam_appid.acf", "r") as f:
-    data = json.load(f)
+def extract_user_language(filename):
+    with open(filename, 'r') as file:
+        data = file.read()
 
-# Change the value of the "Name" key
-data["Name"] = "New Name"
+    user_config_match = re.search(r'"UserConfig"\s*{(.+?)\s*}', data, re.DOTALL)
 
-# Write the changed content back to the file
-with open("steam_appid.acf", "w") as f:
-    json.dump(data, f, indent=4)
+    if user_config_match:
+        user_config_block = user_config_match.group(1)
+
+        language_match = re.search(r'"language"\s+"([^"]+)"', user_config_block)
+
+        if language_match:
+            user_language = language_match.group(1)
+            return user_language
+        else:
+            print("Language not found in UserConfig block.")
+            return None
+    else:
+        print("UserConfig block not found in the file.")
+        return None
+
+if __name__ == "__main__":
+    filename = "appmanifest_10.acf"
+    user_language = extract_user_language(filename)
+    
+    if user_language and user_language.lower() != "english":
+        with open("user_language.cfg", 'w') as output_file:
+            output_file.write(user_language)
+            print(f"User language '{user_language}' has been written to user_language.cfg.")
+    else:
+        print("User language is 'english', ignoring the writing to user_language.cfg.")

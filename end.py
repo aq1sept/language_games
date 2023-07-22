7b986c522c6495d265c2eb457563bdaebc8a61da
aq1sept <vadim-petrosyan@ukr.net>
2023-07-22 19:02:06 +0300

    fix bug if u back to english from another

--- end.py
+++ end.py
@@ -1,10 +1,10 @@
-# VERSION: 2.0
+# VERSION: 2.2
 import os
 import re
 import glob
 
 def extract_user_language(filename):
-    with open(filename, 'r') as file:
+    with open(filename, 'r', encoding='utf-8') as file:
         data = file.read()
 
     user_config_match = re.search(r'"UserConfig"\s*{(.+?)\s*}', data, re.DOTALL)
@@ -24,14 +24,16 @@ def extract_user_language(filename):
         print("UserConfig block not found in the file.")
         return None
 
-steamapps_path = r"C:\Program Files (x86)\Steam\steamapps"
-#steamapps_path = r"C:\Users\admin\Desktop\git\language_games\steamapps_test" FOR TESTING
+steamapps_path = r"C:\Users\admin\Desktop\git\language_games\steamapps_test"
+# steamapps_path = r"C:\Program Files (x86)\Steam\steamapps"
 blacklist_file = "blacklist_appmanifest.cfg"
 
 blacklist = []
 if os.path.exists(blacklist_file):
-    with open(blacklist_file, 'r') as blacklist_file:
+    with open(blacklist_file, 'r', encoding='utf-8') as blacklist_file:
         blacklist = [line.strip() for line in blacklist_file]
+
+english_found = False
 
 for filename in glob.glob(os.path.join(steamapps_path, "appmanifest_*.acf")):
     if os.path.basename(filename) in blacklist:
@@ -41,9 +43,16 @@ for filename in glob.glob(os.path.join(steamapps_path, "appmanifest_*.acf")):
     user_language = extract_user_language(filename)
 
     if user_language and user_language.lower() != "english":
-        with open("user_language.cfg", 'w') as output_file:
+        with open("user_language.cfg", 'w', encoding='utf-8') as output_file:
             output_file.write(user_language)
             print(f"User language '{user_language}' has been written to user_language.cfg.")
+            english_found = False
         break
     else:
         print(f"File: {filename}, User language is 'english', ignoring the writing to user_language.cfg.")
+        english_found = True
+    
+if english_found:
+    with open("user_language.cfg", 'w', encoding='utf-8') as output_file:
+        output_file.write("english")
+        print("User language 'english' has been written to user_language.cfg.")
--- start.py
+++ start.py
@@ -1,47 +1,50 @@
-# VERSION: 2.0
+# VERSION: 2.1
 import os
 import re
-import sys
-import datetime
 
-folder_path = "C:\\Program Files (x86)\\Steam\\steamapps"
+folder_path = r"C:\Users\admin\Desktop\git\language_games\steamapps_test"
+#folder_path = "C:\\Program Files (x86)\\Steam\\steamapps"
 config_file_path = "user_language.cfg"
-log_file_path = "start_log.txt"
+blacklist_file_path = "blacklist_appmanifest.cfg"
 
 if not os.path.isfile(config_file_path):
-    with open(config_file_path, "w") as config_file:
+    with open(config_file_path, "w", encoding="utf-8") as config_file:
         config_file.write("english")
 
-log_file = open(log_file_path, "a")
-
-original_stdout = sys.stdout
-sys.stdout = log_file
-
-def log_with_timestamp(message):
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    log_message = f"[{timestamp}] {message}"
-    print(log_message)
-
-with open(config_file_path, "r") as config_file:
+with open(config_file_path, "r", encoding="utf-8") as config_file:
     config_text = config_file.read().strip()
 
+blacklisted_filenames = set()
+if os.path.isfile(blacklist_file_path):
+    with open(blacklist_file_path, "r", encoding="utf-8") as blacklist_file:
+        for line in blacklist_file:
+            filename = line.strip()
+            if filename:
+                blacklisted_filenames.add(filename)
+
 for filename in os.listdir(folder_path):
-    if filename.startswith("appmanifest_") and filename.endswith(".acf"):
+    if (
+        filename.startswith("appmanifest_")
+        and filename.endswith(".acf")
+        and filename not in blacklisted_filenames
+    ):
         file_path = os.path.join(folder_path, filename)
 
         try:
-            with open(file_path, "r+") as manifest_file:
+            with open(file_path, "r+", encoding="utf-8") as manifest_file:
                 manifest_text = manifest_file.read()
 
-                manifest_text = re.sub(r'("UserConfig"\s*{[\s\S]*?"language"\s*")(.+?)("\s*[\s\S]*?})', fr'\1{config_text}\3', manifest_text, flags=re.DOTALL)
+                manifest_text = re.sub(
+                    r'("UserConfig"\s*{[\s\S]*?"language"\s*")(.+?)("\s*[\s\S]*?})',
+                    fr'\1{config_text}\3',
+                    manifest_text,
+                    flags=re.DOTALL,
+                )
 
                 manifest_file.seek(0)
                 manifest_file.write(manifest_text)
                 manifest_file.truncate()
 
-            log_with_timestamp(f"File {filename} updated.")
+            print(f"File {filename} updated.")
         except FileNotFoundError:
-            log_with_timestamp(f"File {filename} not found. Skipping.")
-
-sys.stdout = original_stdout
-log_file.close()
+            print(f"File {filename} not found. Skipping.")

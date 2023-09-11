f74d380a1e03aa637e8137c069c8a0a1daf6dd65
aq1sept <vadim-petrosyan@ukr.net>
2023-09-11 16:38:45 +0300

    del trash

--- start.py
+++ start.py
@@ -4,7 +4,6 @@ import logging
 import datetime
 
 folder_path = r"C:\Program Files (x86)\Steam\steamapps"
-#folder_path = r"C:\Program Files (x86)\Steam\steamapps"
 config_file_path = "user_language.cfg"
 blacklist_file_path = "blacklist_appmanifest.cfg"
 log_folder_path = r"C:\Users\user\boosteroid-experience\logs\language_games_logs"

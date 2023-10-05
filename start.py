bbde7cc32f6c17b5e1cb65e7de5e2f56564877bc
aq1sept <vadim-petrosyan@ukr.net>
2023-10-05 11:42:32 +0300

    added logger

--- start.py
+++ start.py
@@ -8,25 +8,6 @@ config_file_path = "user_language.cfg"
 blacklist_file_path = "blacklist_appmanifest.cfg"
 log_folder_path = r"C:\Users\user\boosteroid-experience\logs\language_games_logs"
 
-file_list = [f for f in os.listdir(folder_path) if f.startswith("appmanifest_") and f.endswith(".acf")]
-
-pattern = re.compile(r'("LastOwner"\s+")\d+"')
-
-for file_name in file_list:
-    file_path = os.path.join(folder_path, file_name)
-    
-    try:
-        with open(file_path, 'r') as file:
-            content = file.read()
-    except FileNotFoundError:
-        print(f"File not found: {file_path}")
-        continue
-
-    new_content = pattern.sub(r'\1"', content)
-
-    with open(file_path, 'w') as file:
-        file.write(new_content)
-
 if not os.path.exists(log_folder_path):
     os.makedirs(log_folder_path)
 
@@ -37,14 +18,42 @@ logger = logging.getLogger()
 
 logger.info(f"Start.exe is launched. Version: 1.00")
 
+file_list = [f for f in os.listdir(folder_path) if f.startswith("appmanifest_") and f.endswith(".acf")]
+
+pattern = re.compile(r'("LastOwner"\s+")\d+"')
+
+for file_name in file_list:
+    file_path = os.path.join(folder_path, file_name)
+    
+    try:
+        with open(file_path, 'r') as file:
+            content = file.read()
+    except FileNotFoundError as e:
+        logging.error(f"File not found: {file_path}. Error: {e}")
+        continue
+    except Exception as e:
+        logging.error(f"Error reading file {file_path}: {e}")
+        continue
+
+    new_content = pattern.sub(r'\1"', content)
+
+    try:
+        with open(file_path, 'w') as file:
+            file.write(new_content)
+    except Exception as e:
+        logging.error(f"Error writing to file {file_path}: {e}")
+
 def read_file_lines(file_path):
     with open(file_path, "r", encoding="utf-8", newline='\n') as file:
         return [line.strip() for line in file]
 
 if not os.path.isfile(config_file_path):
-    with open(config_file_path, "w", encoding="utf-8", newline='\n') as config_file:
-        config_file.write("english")
-    logger.info(f"Created {config_file_path} with default language 'english'.")
+    try:
+        with open(config_file_path, "w", encoding="utf-8", newline='\n') as config_file:
+            config_file.write("english")
+        logger.info(f"Created {config_file_path} with default language 'english'.")
+    except Exception as e:
+        logger.error(f"Error creating {config_file_path}: {e}")
 
 config_text = read_file_lines(config_file_path)[0]
 
@@ -73,5 +82,7 @@ for filename in os.listdir(folder_path):
                 manifest_file.truncate()
 
             logger.info(f"Updated language in file {filename}.")
-        except FileNotFoundError:
-            logger.warning(f"File {filename} not found. Skipping.")
+        except FileNotFoundError as e:
+            logger.warning(f"File {filename} not found. Skipping. Error: {e}")
+        except Exception as e:
+            logger.error(f"Error updating file {filename}: {e}")
